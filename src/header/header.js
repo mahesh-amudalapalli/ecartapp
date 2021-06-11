95624b3e08ea248b33615e5ecbd92e10ad9626c0
mahesh-amudalapalli <mahesh.mrcet@gmail.com>
2021-06-11 10:50:09 +0530

    second commit
    changes header names

--- src/header/header.js
+++ src/header/header.js
@@ -24,8 +24,8 @@ const Header = (props) => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link as={Link} onClick={() => props.SetReadOnlyHandler(true)} className={splitLocation[1] === "home" ? "active" : ""} to='/home'>Home</Nav.Link>
-                        <Nav.Link as={Link} onClick={() => props.SetReadOnlyHandler(true)} className={splitLocation[1] === "aboutus" ? "active" : ""} to='/aboutus'>AboutUs</Nav.Link>
+                        <Nav.Link as={Link} onClick={() => props.SetReadOnlyHandler(true)} className={splitLocation[1] === "home" ? "active" : ""} to='/home'>Home-Git</Nav.Link>
+                        <Nav.Link as={Link} onClick={() => props.SetReadOnlyHandler(true)} className={splitLocation[1] === "aboutus" ? "active" : ""} to='/aboutus'>AboutUs-Git</Nav.Link>
                         <NavDropdown className={splitLocation[1].startsWith("Department") ? "active" : ""} title="Department" id="basic-nav-dropdown">
                             <NavDropdown.Item onClick={() => props.SetReadOnlyHandler(true)} as={Link} to="/Department/Electrical">Electrical</NavDropdown.Item>
                             <NavDropdown.Item onClick={() => props.SetReadOnlyHandler(true)} as={Link} to="/Department/Electronics">Electronics</NavDropdown.Item>
